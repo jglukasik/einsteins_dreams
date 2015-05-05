cf6b3fbb44bd436d871479a34944c5ccef497611
Joseph Lukasik <jglukasik@wisc.edu>
2015-05-05 10:52:09 -0500

    Call program to create html doc of todays dream

--- dream_eater.pl
+++ dream_eater.pl
@@ -5,6 +5,7 @@
 use warnings;
 use strict;
 
+use HTML::Template;
 use Time::Piece;
 use Data::Dumper;
 
@@ -12,18 +13,47 @@ open(my $fh, "<", 'ed.txt') or die "can't open file: $!";
 
 my %book;
 my $day = '';
+my $dream = '';
+while (<$fh>) {
+  if (/^• PROLOGUE/){
+    last;
+  }
+}
 
 while (<$fh>) {
   chomp;
-  $_ =~ s/\d{1,2,3}\/203//g;
+  $_ =~ s/• //g;
   $_ =~ s/^.*$//g;
-  if (/^\d{1,2} (APRIL|April|May|June) 1905/){
-    my $d = Time::Piece->strptime($_,"%d %B %Y");
-    $day = $d->mdy;
+  if (/^(\d{1,2} (APRIL|April|May|June) 1905)|INTERLUDE|EPILOGUE/){
+
+    if ($dream ne ''){
+      $book{$day} = $dream;
+      $dream = '';
+    }
+
+    if (/INTERLUDE|EPILOGUE/){
+      $day = $_;
+    } else {
+      my $d = Time::Piece->strptime($_,"%d %B %Y");
+      $day = $d->mdy;
+    }
+
+    $book{$day} = ();
     next;
   }
-  push $_, @{ $book{$day} }; 
+  if (!/^\s*$/){
+    $dream = $dream . ' ' . $_;
+  }
 }
 
-#print Dumper \%book;
-#print $book{"05-29-1905"};
+my $todays_dream;
+if (defined $book{"$ARGV[0]-1905"}){
+  $todays_dream = $book{"$ARGV[0]-1905"};
+} else {
+  $todays_dream = "No dream today...";
+}
+
+my $template = HTML::Template->new(filename => 'dream_catcher.html');
+$template->param(DREAM => $todays_dream);
+print $template->output;
+
