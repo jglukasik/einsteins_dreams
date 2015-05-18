6e48b611df79801dbb1392421c2529753aed0764
Joseph Lukasik <jglukasik@wisc.edu>
2015-05-18 01:32:40 -0500

    Added an email form that will insert into the database

--- dreamer.hs
+++ dreamer.hs
@@ -13,19 +13,23 @@ import Database.Persist.Sqlite
 import Control.Monad.IO.Class (liftIO)
 import System.Locale
 import Data.Time
-import Data.Text (Text)
+import Data.Text (Text, unpack)
 import Yesod
 import Yesod.Static
+import Yesod.Form.Jquery
 import Yesod.Default.Handlers
 import Text.Hamlet
 import Control.Monad.Trans.Resource (runResourceT)
 import Control.Monad.Logger (runStderrLoggingT)
+import Control.Applicative ((<$>))
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 Dream
   date UTCTime
   content Text
   deriving Show
+SavedEmail
+  address String
 |]
 
 staticFiles "static"
@@ -35,8 +39,15 @@ data App = App
     , pool :: ConnectionPool
     }
 
+data Email = Email
+    { emailAddress :: Text
+    }
+    deriving Show
+
 mkYesod "App" [parseRoutes|
 /            HomeR    GET
+/email       EmailR   POST
+
 /static      StaticR  Static getStatic
 /favicon.ico FaviconR GET
 |]
@@ -48,6 +59,11 @@ instance Yesod App where
         contents
       withUrlRenderer $(hamletFile "dream_world.hamlet")
 
+instance RenderMessage App FormMessage where
+  renderMessage _ _ = defaultFormMessage
+
+instance YesodJquery App
+
 instance YesodPersist App where
     type YesodPersistBackend App = SqlBackend
 
@@ -55,17 +71,48 @@ instance YesodPersist App where
       App static pool <- getYesod
       runSqlPool action pool
 
+emailForm :: Html -> MForm Handler (FormResult Email, Widget)
+emailForm = renderDivs $ Email
+    <$> areq emailField "Email address:  " Nothing
+
+postEmailR :: Handler Html
+postEmailR = do
+  ((formResult, formWidget), encType) <- runFormPost emailForm
+  case formResult of
+    FormSuccess email -> do
+        runSqlite "dream_catcher.db" $ do
+          insert . SavedEmail . unpack . emailAddress $ email
+        defaultLayout [whamlet| <p>Success! The email address:
+                                <p><code>#{ emailAddress email }</code>
+                                <p>will be emailed on days with a new dream
+                              |]
+    _ -> defaultLayout [whamlet| <p>Invalid input |]
+
 getHomeR :: Handler Html
 getHomeR = do
   dateTime <- liftIO dateIn1905
   maybeDream <- runDB $ selectFirst [DreamDate ==. dateTime] []
+  (formWidget, encType) <- generateFormPost emailForm
   defaultLayout [whamlet|
                   <body>
                     <h3>#{showGregorian (utctDay (dateTime))}
                     $maybe Entity dreamid dream <- maybeDream
                       <p>#{dreamContent dream}
                     $nothing
-                      <h1>No dream today...
+                      <p>No dream today...
+                    <br>
+                    <br>
+                    <div class="jumbotron">
+                      <form method=post action=@{EmailR} enctype=#{encType}>
+                        ^{formWidget}
+                        <button>Submit
+                        <br>
+                        <br>
+                        Want to be notified on days with a new dream? Then enter
+                        your email here. I promise to never send you anything
+                        other than alerts for these dreams (30 total between the
+                        months of April and June), and to never share your email
+                        with anyone else.
                 |]
 
 dateIn1905 :: IO UTCTime
