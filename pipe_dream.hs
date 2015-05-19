146cbd8db3cc24fb68de0e09dbc7df1508aeb93b
Joseph Lukasik <jglukasik@wisc.edu>
2015-05-18 22:16:45 -0500

    Pipe dream will send alert (but text only) emails to emails in db

--- pipe_dream.hs
+++ pipe_dream.hs
@@ -8,9 +8,14 @@
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
 import           Control.Monad.IO.Class (liftIO)
+import           Data.Text (pack)
 import           Database.Persist
 import           Database.Persist.Sqlite
 import           Database.Persist.TH
+import           Network.Mail.Mime
+import           Network.Mail.Client.Gmail
+
+import Credentials (googleAppPassword)
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 SavedEmail
@@ -22,5 +27,16 @@ main :: IO ()
 main = runSqlite "dream_diary.db" $ do
     runMigration migrateAll
     emailRows <- selectList [SavedEmailAddress !=. ""] []
-    let emails = map (savedEmailAddress . entityVal) emailRows
-    liftIO $ mapM_ print emails
+    let emails = map (pack . savedEmailAddress . entityVal) emailRows
+    liftIO $ mapM_ sendAlertEmail emails
+
+sendAlertEmail recepient = sendGmail "jglukasik"
+                                     googleAppPassword
+                                     (Address (Just "Joseph Lukasik") "jglukasik@gmail.com")
+                                     [Address Nothing recepient]
+                                     []
+                                     []
+                                     "New Einstein's Dream"
+                                     "Check out the new dream at http://ed.jgl.me"
+                                     []
+                                     10000000
