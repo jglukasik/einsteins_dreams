8802f294f50917ff21ab705afa1ca0386fbad55f
Joseph Lukasik <jglukasik@wisc.edu>
2015-06-10 23:19:37 -0700

    Send out the dream content in the email

--- pipe_dreams.hs
+++ pipe_dreams.hs
@@ -8,13 +8,15 @@
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
 import           Control.Monad.IO.Class (liftIO)
-import           Data.Text (Text, pack)
+import           Data.Text (Text, pack, unpack, concat)
+import           Data.Text.Lazy (fromStrict)
 import           Data.Time
 import           Database.Persist
 import           Database.Persist.Sqlite
 import           Database.Persist.TH
 import           Network.Mail.Mime
 import           Network.Mail.Client.Gmail
+import           Prelude hiding (concat)
 import           System.Locale
 
 import Credentials (googleAppPassword)
@@ -37,19 +39,26 @@ main = do
       Nothing -> return ()
       Just todaysDream -> runSqlite "dream_diary.db" $ do
         emailRows <- selectList [SavedEmailAddress !=. ""] []
-        let emails = map (pack . savedEmailAddress . entityVal) emailRows
-        liftIO $ mapM_ sendAlertEmail emails
+        let emails = map (pack . savedEmailAddress . entityVal) emailRows 
+        let dream = fromStrict . formatDream . dreamContent . entityVal $ todaysDream
+        liftIO $ mapM_ (sendAlertEmail dream) emails
 
-sendAlertEmail recepient = sendGmail "jglukasik"
-                                     googleAppPassword
-                                     (Address (Just "Joseph Lukasik") "jglukasik@gmail.com")
-                                     [Address Nothing recepient]
-                                     []
-                                     []
-                                     "New Einstein's Dream"
-                                     "Check out today's dream at http://ed.jgl.me"
-                                     []
-                                     10000000
+sendAlertEmail body recepient = sendGmail "jglukasik"
+                                          googleAppPassword
+                                          (Address (Just "Joseph Lukasik") "jglukasik@gmail.com")
+                                          [Address Nothing recepient]
+                                          []
+                                          []
+                                          "New Einstein's Dream"
+                                          body
+                                          []
+                                          10000000
+
+formatDream input = concat [ "Einstein's dream for today...\n\n"
+                           , input
+                           , "\n\nFrom http://ed.jgl.me \n"
+                           , "Don't wan't these emails anymore? Enter your address again on the site to remove yourself from the mailing list"
+                           ]
 
 dateIn1905 :: IO UTCTime
 dateIn1905 = do 
